f3599d9b86cffd64c6b5cd506797bc6bc324c49b
Janos Perge <janos.perge@optimonk.com>
2023-11-13 09:00:38 +0100

    create boilerplate

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'quotes': ['error', 'single'],
+    // we want to force semicolons
+    'semi': ['error', 'always'],
+    // we use 2 spaces to indent our code
+    'indent': ['error', 2],
+    // we want to avoid extraneous spaces
+    'no-multi-spaces': ['error']
+  }
+};
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+require('./config');
+
+const { User } = require('./models');
+
+const typeDefs = gql`
+    type User {
+        id: ID!
+        userName: String
+        email: String
+    }
+    type Query {
+        getUsers: [User]
+    }
+    type Mutation {
+        addUser(userName: String!, email: String!): User
+    }
+`;
+
+const resolvers = {
+  Query: {
+    getUsers: async () => await User.find({}).exec()
+  },
+  Mutation: {
+    addUser: async (_, args) => {
+      try {
+        let response = await User.create(args);
+        return response;
+      } catch(e) {
+        return e.message;
+      }
+    }
+  }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const app = express();
+
+
+server.start().then(res => {
+  server.applyMiddleware({ app, path: '/' });
+  
+  app.listen({ port: 4000 }, () => 
+    console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  );  
+});
